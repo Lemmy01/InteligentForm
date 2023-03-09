a43dd29cb093dff121eafde666184a36a20c3088
George Luta <yotrevorgxg@gmail.com>
2023-03-09 17:29:21 +0200

    fix : sign_up pop pages

--- lib/features/authentication/presentation/pages/login_page/login_page.dart
+++ lib/features/authentication/presentation/pages/login_page/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inteligent_forms/features/authentication/presentation/pages/home_page/home_page.dart';
 import 'package:inteligent_forms/features/authentication/presentation/pages/login_page/widgets/login_page_body.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,9 +7,20 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-      body: const LoginPageBody(),
+    return WillPopScope(
+      onWillPop: () async {
+        await Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ),
+        );
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        body: const LoginPageBody(),
+      ),
     );
   }
 }
--- lib/features/authentication/presentation/pages/sign_up_page/sign_up_page.dart
+++ lib/features/authentication/presentation/pages/sign_up_page/sign_up_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:inteligent_forms/features/authentication/presentation/pages/app_bottom_bar.dart';
+import 'package:inteligent_forms/features/authentication/presentation/pages/login_page/login_page.dart';
 import 'package:inteligent_forms/features/authentication/presentation/pages/sign_up_page/widgets/sign_up_page_body.dart';
 
 class SignUpPage extends StatelessWidget {
@@ -8,16 +9,27 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.secondary,
-      body: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const AppBottomBar();
-          }
-          return const SignUpPageBody();
-        },
+    return WillPopScope(
+      onWillPop: () async {
+        await Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const LoginPage(),
+          ),
+        );
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        body: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const AppBottomBar();
+            }
+            return const SignUpPageBody();
+          },
+        ),
       ),
     );
   }
